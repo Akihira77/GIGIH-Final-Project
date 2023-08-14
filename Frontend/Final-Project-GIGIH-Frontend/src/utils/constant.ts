394d9cca6867c229ce361490b8cf84c84984b67e
Akihira77 <tik.dikawahyu123@gmail.com>
2023-08-14 13:36:28 +0700

    setup final frontend project

--- Frontend/Final-Project-GIGIH-Frontend/src/utils/constant.ts
+++ Frontend/Final-Project-GIGIH-Frontend/src/utils/constant.ts
@@ -3,7 +3,7 @@ export const reducerCases = {
 };
 
 // export const API_URL = "http://localhost:1337/api";
-export const API_URL = process.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export const expiresOneHour = new Date(
   Date.now() + 60 * 60 * 1000
